2cdda2ee6a84bc7cb142ec0bffa4513d8294fc59
秦真 <739694218@qq.com>
2020-12-22 13:38:06 +0800

    bugfix

--- main.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: Do not edit
- * @Date: 2020-12-18 12:26:59
- * @LastEditors: 秦真
- * @LastEditTime: 2020-12-18 12:50:09
- * @Description: 
- * @FilePath: \autorun\main.js
- */
-toastLog('Hello, Auto.js ' + app.autojs.versionName + ' ' + app.getPackageName('支付宝'));
